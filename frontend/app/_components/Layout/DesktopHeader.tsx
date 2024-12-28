d4727ad1f55dd1c4d2c8f7921d22b75cc66aab3b
Seowon Choi <chltjdnjs529@gmail.com>
2024-12-28 15:36:05 +0900

    feat: [FE] Header desktop/mobile 컴포넌트 분리

---
+++ frontend/app/_components/Layout/DesktopHeader.tsx
@@ -0,0 +1,103 @@
+import { childMenu, HEADER_HEIGHT, MENU_INFO } from '@/constants';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import styles from './Layout.module.css';
+
+interface DesktopHeaderProps {
+  hamburger: boolean;
+  setHamburger: (hamburger: boolean) => void;
+  handleContactMenu: () => void;
+  user: any;
+  isLoading: boolean;
+  logout: () => void;
+}
+
+const DesktopHeader = ({hamburger, setHamburger, handleContactMenu, user, isLoading, logout}: DesktopHeaderProps) => {
+
+  // 스크롤 높이 감지
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setHamburger(false);
+      }
+    };
+
+    // 데스크탑 화면에서만 스크롤 이벤트 추가
+    if (window.innerWidth >= 1024) {
+      window.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [setHamburger]);
+
+  const ChildrenMenus = ({childrenMenus}:{childrenMenus:Array<childMenu>}) => {
+    return <div className={`flex flex-col items-center rounded-3xl absolute ${styles.childrenMenuBox} slide-in-blurred-top`}
+    style={{top: HEADER_HEIGHT - 15}}>
+      {childrenMenus.map(item => 
+      <Link href={item.link} key={`children-menu-${item.title}`} className={`${styles.childMenu} flex items-center justify-center`}>
+        <span>{item.title}</span>
+      </Link>)}
+    </div>
+  }
+
+  return <div className="hidden lg:block">
+    <div 
+      className={`${styles.header} w-full flex items-center justify-between fixed inset-x-0 top-0`}
+      style={{height: HEADER_HEIGHT}}
+    >
+      <div className="flex">
+        <button 
+          className={`${styles.menuHamburger} hover:bg-zinc-100 active:bg-zinc-200 rounded-full flex items-center justify-center`}
+          onClick={()=>setHamburger(!hamburger)}
+        >
+          <img src="/images/icons/hamburger_bar.png" alt="menu" width={22} className="opacity-80"/>
+        </button>
+        <Link href={'/'} className="flex items-center ml-8">
+          <img src="/images/logos/logo_green.png" alt="logo" width={34} style={{marginTop: -4}}/>
+          <span className="NanumSquare xl:text-2xl lg:text-xl font-extrabold ml-4 text-green-1">
+            조재현 수학학원
+          </span>
+        </Link>
+      </div>
+      <div className="lg:flex items-center hidden">
+        {Object.values(MENU_INFO).map(item => {
+          const [showChildren, setShowChildren] = useState<boolean>(false);
+          return <div 
+            key={`menu-${item.title}`} 
+            onMouseLeave={()=>setShowChildren(false)}
+            className="relative"
+          >
+            <button 
+              className={`${styles.menu} xl:text-lg lg:text-base font-semibold text-gray-600 hover:text-green-1`} 
+              onClick={item.link ? handleContactMenu : ()=>{}}
+              onMouseOver={item.link ? ()=>{} : ()=>setShowChildren(true)}
+            >
+              {item.title}
+            </button>
+            {showChildren && item.children && <ChildrenMenus childrenMenus={item.children || []}></ChildrenMenus>}
+          </div>
+        })}
+        {!isLoading && <>
+          <Link 
+            href={user ? '/user' : '/auth/login'} 
+            className="flex items-center py-2 px-4 ml-6 hover:text-green-1 hover:cursor-pointer"
+          >
+            <i title={user ? "마이페이지" : "로그인"} className="far fa-user"></i>
+          </Link>
+          {user && (
+            <div 
+              className="flex items-center py-2 px-4 ml-6 hover:text-green-1 hover:cursor-pointer" 
+              onClick={logout}
+            >
+              <i title="로그아웃" className="fas fa-sign-out-alt"></i>
+            </div>
+          )}
+        </>}
+      </div>
+    </div>
+  </div>
+}
+
+export default DesktopHeader;
---
+++ frontend/app/_components/Layout/MobileHeader.tsx
@@ -0,0 +1,109 @@
+import { CONTACT_INFO, HEADER_HEIGHT_MOBILE, MENU_INFO } from '@/constants';
+import Link from 'next/link';
+import { useState } from 'react';
+import styles from './Layout.module.css';
+
+interface MobileHeaderProps {
+  hamburger: boolean;
+  setHamburger: (hamburger: boolean) => void;
+  handleContactMenu: () => void;
+}
+
+const MobileHeader = ({hamburger, setHamburger, handleContactMenu}: MobileHeaderProps) => {
+  return <div className="flex lg:hidden w-screen fixed inset-x-0 top-0 z-50">
+    <div 
+      className={`${styles.header} w-screen flex items-center justify-between fixed inset-x-0 top-0`} 
+      style={{height: HEADER_HEIGHT_MOBILE}}
+    >
+      <img src="/images/logos/logo_green.png" alt="logo" width={30} style={{marginTop: -3}}/>
+      <Link href={'/'} className="NanumSquare text-xl font-bold text-green-1">
+        조재현 수학학원
+      </Link>
+      <button 
+        className={`${styles.menuHamburger}`}
+        onClick={()=>setHamburger(!hamburger)}
+      >
+        <img src="/images/icons/hamburger_bar.png" alt="menu" width={25}/>
+      </button>
+    </div> 
+    <div 
+      className={`${styles.mobileMenuBg} w-screen fixed inset-x-0 top-0 bg-darkgray-1`}
+      style={{
+        backgroundColor: hamburger ? 'rgba(0,0,0,0.25)' : 'transparent',
+      }}
+    >
+        <div 
+          className={`${styles.mobileMenuBox} w-screen fixed inset-x-0 flex flex-col`}
+          style={{
+            top: hamburger ? 50 : '-100%'
+          }}
+        >
+          <div className="overflow-scroll" style={{height: "calc(100% - 5rem)"}}>
+            {Object.values(MENU_INFO).map(parentMenu => {
+              const [menuOpened, setMenuOpened] = useState<boolean>(false);
+              const handleMenuClick = () => {
+                if (parentMenu.children) {
+                  setMenuOpened(!menuOpened)
+                } else {
+                  handleContactMenu()
+                }
+              }
+              return <div key={`mobile-parent-menu-${parentMenu.sort}`}>
+                <div className="overflow-hidden" style={{
+                  height: menuOpened ? `calc(4.875rem + ${parentMenu.children!.length * 2.875}rem)` : '3.5rem',
+                  transition: 'height 0.25s ease-in-out'
+                }}>
+                  <div 
+                    className={`${styles.mobileParentMenu} w-full h-14 rounded-xl px-6 flex items-center justify-between mb-1`}
+                    style={{
+                      backgroundColor: menuOpened ? '#F0F0F0' : 'transparent'
+                    }}
+                    onClick={handleMenuClick}
+                  > 
+                    <span className="text-green-1 text-xl leading-none font-bold">
+                      {parentMenu.title}
+                    </span>
+                    {parentMenu.children && <img 
+                      src="/images/icons/arrow_rounded.png" 
+                      alt="close" className={`filter-green-1 ${!menuOpened && 'rotate-180'}`}
+                      width={20} 
+                    />}
+                  </div>
+                  <div className="pb-2.5 pt-1.5">
+                    {parentMenu.children?.map(childMenu => 
+                      <Link
+                        href={childMenu.link}
+                        key={`mobile-child-menu-${childMenu.sort}`} 
+                        className='py-3.5 pl-7 flex items-center'
+                      >
+                        <div className="w-2 h-2 rounded-full bg-yellow-1 mr-3.5"></div>
+                        <div className="text-lg leading-none decoration-neutral-700">
+                          {childMenu.title}
+                        </div>
+                      </Link>
+                    )}  
+                  </div>
+                </div>
+              </div>
+            })}              
+          </div>
+          <div className="w-10/12 h-20 rounded-3xl bg-green-gradient absolute bottom-12 left-2/4 flex items-center justify-evenly" style={{transform: 'translateX(-50%)'}}>
+            {Object.values(CONTACT_INFO).map(contact => (
+              <a 
+                href={contact.link} 
+                target={contact.link.startsWith('/') ? undefined : "_blank"} 
+                key={`mobile-contact-${contact.sort}`}
+              > 
+                <img src={contact.icon} alt={contact.title} width={30} className="invert"/>
+              </a>
+            ))}
+          </div>
+          <div className={`${styles.mobileMenuClose} w-11 h-11 bg-green-3 rounded-full flex items-center justify-center`} onClick={()=>setHamburger(false)}>
+            <img src="/images/icons/arrow_rounded.png" alt="close" className="invert"/>
+          </div>
+        </div>
+    </div>
+  </div>
+}
+
+export default MobileHeader;
