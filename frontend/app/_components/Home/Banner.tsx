f6f81b6344bed0ff35083d51f2c071cca4a1145f
Seowon Choi <chltjdnjs529@gmail.com>
2025-01-05 18:10:04 +0900

    refactor: [FE] update HomeBanner component with TypeScript improvements and enhanced mobile responsiveness

--- frontend/app/_components/Home/Banner.tsx
+++ frontend/app/_components/Home/Banner.tsx
@@ -3,24 +3,23 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import PlusIconButton from "../CustomButtons/PlusIconButton";
-import styles from './Layout.module.css';
 
 const HomeBanner = () => {
   const router = useRouter();
   const [activeBanner, setActiveBanner] = useState<Array<boolean>>([true, false, false, false]);
 
-  type banner = {
+  type Banner = {
     sort: number,
-    title: string
-    subtitle: string
-    color: string
-    backgroundColor: string
-    imgPath: string
-    imgHeight: number
+    title: string,
+    subtitle: string,
+    color: string,
+    backgroundColor: string,
+    imgPath: string,
+    imgHeight: number,
     link: string
   }
 
-  const bannerList:banner[] = [
+  const bannerList: Banner[] = [
     {
       sort: 1,
       title: "조재현\n수학학원",
@@ -63,85 +62,90 @@ const HomeBanner = () => {
     },
   ];
 
-  const MobileBannerCard = (props: { item: banner }) => {
-    const { item } = props;
-
-    return <div 
-      className={`h-[300px] w-[280px] mx-2.5 rounded-xl relative overflow-x-scroll flex-shrink-0 snap-center`}
-      style={{
-        background: item.backgroundColor, 
-        boxShadow: '8px 8px 24px 0px rgba(0, 0, 0, 0.10), -8px -8px 24px 0px rgba(255, 255, 255, 0.10)',
-      }}
-    >
-      <div className="mt-7 mx-6 flex justify-between">
-        <div className="leading-snug">
-          <div className="text-base font-bold NanumSquare mb-1">
-            {item.subtitle}
+  const MobileBannerCard = ({ item }: { item: Banner }) => {
+    return (
+      <div 
+        className={`h-[300px] w-[280px] mx-2.5 rounded-xl relative overflow-x-scroll flex-shrink-0 snap-center`}
+        style={{
+          background: item.backgroundColor, 
+          boxShadow: '8px 8px 24px 0px rgba(0, 0, 0, 0.10), -8px -8px 24px 0px rgba(255, 255, 255, 0.10)',
+        }}
+      >
+        <div className="mt-7 mx-6 flex justify-between">
+          <div className="leading-snug">
+            <div className="text-base font-bold NanumSquare mb-1">
+              {item.subtitle}
+            </div>
+            <div className="text-2xl font-bold" style={{color: item.color}}>
+              {item.title}
+            </div>
           </div>
-          <div className="text-2xl font-bold" style={{color: item.color}}>
-            {item.title}
-          </div>
+          <PlusIconButton color={item.color} size={20}/>
         </div>
-        <PlusIconButton color={item.color} size={20}/>
+        <div className="absolute bottom-0 flex justify-center w-full h-[200px] bg-no-repeat bg-top" style={{
+          backgroundImage: `url(${item.imgPath})`,
+          backgroundSize: item.sort === 2 ? '90%' : 'cover',
+        }}>
+          {/* <img src={item.imgPath}/> */}
+        </div>
       </div>
-      <div className="absolute bottom-0 flex justify-center w-full h-[200px] bg-no-repeat bg-top" style={{
-        backgroundImage: `url(${item.imgPath})`,
-        backgroundSize: item.sort === 2 ? '90%' : 'cover',
-      }}>
-        {/* <img src={item.imgPath}/> */}
-      </div>
-    </div>
+    );
   }
 
   return <>
     {/* PC Version */}
     <div className="lg:flex hidden">
-      {bannerList.map((banner, index) => {
-        const active = activeBanner[index];
-        return <div
-          key={`home-banner-${banner.title}`} 
-          className={`${styles.banner} h-[480px] rounded-[30px] cursor-pointer transition-all duration-300 pt-10 flex flex-col xl:items-start w-1/6 pd-0 overflow-hidden NanumSquare ${active ? styles.active : 'xl:pl-5 lg:pl-0 lg:items-center'}`}
-          style={{
-            background: banner.backgroundColor,
-            color: banner.color,
-          }}
-          onMouseOver={()=>setActiveBanner(prevState => prevState.map((_, i) => i === index))}
-          onClick={()=>router.push(banner.link)}
-        >
-          <div>
-            <div className="xl:text-lg lg:text-base font-bold decoration-slate-600">
-              {
-                index == 0 && !active? 
-                <span dangerouslySetInnerHTML={{ __html: banner.subtitle.replace(' ', '<br />') }} /> : 
-                banner.subtitle
-              }
+      <div className="flex w-full gap-10">
+        {bannerList.map((banner, index) => {
+          const active = activeBanner[index];
+          return (
+            <div
+              key={`home-banner-${banner.title}`} 
+              className={`relative h-[480px] rounded-[30px] cursor-pointer transition-all duration-300 flex flex-col xl:items-start pd-0 overflow-hidden NanumSquare ${active ? "w-3/6 pt-10" : "w-1/6 pt-6"}`}
+              style={{
+                background: banner.backgroundColor,
+                color: banner.color,
+              }}
+              onMouseOver={() => setActiveBanner(prevState => prevState.map((_, i) => i === index))}
+              onClick={() => router.push(banner.link)}
+            >
+              <div
+                className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full transition-all duration-300" 
+                style={{
+                  width: active && index === 1 ? "412px" : '100%',
+                  height: banner.imgHeight,
+                  bottom: !active ? 
+                    (index === 0 ? '-52px': 
+                    index === 2 ? '-30px':
+                    index === 3 ? '-16px' : 
+                    0) : 0, 
+                }}
+              >
+                <img src={banner.imgPath} alt={banner.title} className="w-full h-full object-cover"/>
+              </div>
+              <div className={`${active ? "ml-[50px]" : index === 1 ? "lg:ml-[8px] xl:ml-[18px] 2xl:ml-6" : "lg:ml-4 xl:ml-7"} transition-all duration-300`}>
+                <div className={`xl:text-lg lg:text-base font-bold text-black ${active ? "whitespace-nowrap" : ""}`}>
+                  {
+                    index == 0 && !active? 
+                    <span dangerouslySetInnerHTML={{ __html: banner.subtitle.replace(' ', '<br />') }} /> : 
+                    banner.subtitle
+                  }
+                </div>
+                <div className={`xl:text-2xl lg:text-xl font-extrabold mt-2.5 ${active ? "whitespace-nowrap" : ""}`}>
+                  {
+                    !active? 
+                    <span dangerouslySetInnerHTML={{ __html: banner.title.replace('\n', '<br />') }} /> : 
+                    banner.title
+                  }
+                </div>          
+              </div>
+              <div className={`absolute top-10 right-10 transition-opacity duration-300 fade-in ${active ? "opacity-100" : "opacity-0"}`}>
+                <PlusIconButton color={banner.color} size={30}/>
+              </div>
             </div>
-            <div className="xl:text-2xl lg:text-xl font-extrabold mt-2.5">
-              {
-                !active? 
-                <span dangerouslySetInnerHTML={{ __html: banner.title.replace('\n', '<br />') }} /> : 
-                banner.title
-              }
-            </div>          
-          </div>
-          <div className="plusButton w-7 h-7 rounded-full flex items-center justify-center absolute top-10 right-9 transition-opacity duration-300 fade-in" style={{background: banner.color, opacity: active ? 1 : 0}}>
-            <img src="/images/icons/plus_icon.png" alt="" width={15.38} className="invert"/>
-          </div>
-          <div 
-            className="absolute w-full flex justify-center" 
-            style={{
-              left: 0, 
-              bottom: !active ? 
-                (index === 0 ? '-52px': 
-                index === 2 ? '-30px':
-                index === 3 ? '-16px' : 
-                0) : 0, 
-              transition: 'bottom 0.3s ease'}}
-          >
-            <img src={banner.imgPath} alt={banner.title} style={{height: banner.imgHeight}}/>
-          </div>
-        </div>
-      })}
+          );
+        })}
+      </div>
     </div>  
 
     {/* Mobile Version */}
