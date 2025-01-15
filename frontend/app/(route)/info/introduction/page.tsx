6a7e4a10385c1e84c39ed19e9e7564dadd639ded
Seowon Choi <chltjdnjs529@gmail.com>
2025-01-15 15:11:50 +0900

    feat: [FE] enhance IntroductionPage with new layout and image integration; update ReactiveButton to support full-width option for improved responsiveness

--- frontend/app/(route)/info/introduction/page.tsx
+++ frontend/app/(route)/info/introduction/page.tsx
@@ -1,5 +1,6 @@
 import BlockQuote from "@/app/_components/BlockQuote/BlockQuote";
 import Title from "@/app/_components/Title/Title";
+import Image from "next/image";
 
 const SchoolList = ({type, schools}: {type: string, schools: string[]}) => {
   return <div className="p-3 sm:px-[20px] sm:py-[15px] rounded-[25px] sm:rounded-full bg-blue-5 flex flex-col sm:flex-row items-center gap-5 border border-blue-1 shadow-2">
@@ -59,30 +60,39 @@ const IntroductionPage = () => {
   ];
 
   return <div>
-    <Title title="학원 소개" color="blue"/>
-    <section className="mb-20 flex flex-col md:flex-row gap-[30px]">
-      <div className="min-w-[300px] min-h-[400px] bg-blue-1 rounded-[20px] shadow-2">
-      </div>
-      <div className="flex-1 flex flex-col justify-between py-2.5">
-        <p className="text-lg leading-relaxed mb-2.5">
-          조재현수학은 <strong>{currentYear - 2005}년 이상의 학원 운영 경력</strong>을 바탕으로 초중등 학생들에게 최상의 맞춤형 수학 교육을 제공합니다. <br/>
-          단순히 수학 성적 향상만을 목표로 하지 않고, 아이들이 스스로 문제를 해결하며 <strong>깊이 있는 사고력</strong>을 키울 수 있도록 돕는 데 중점을 두고 있습니다.
-        </p>
-        <p className="text-lg leading-relaxed mb-2.5">
-          유아교육과 출신의 원장 선생님이 초중등 최상위권 학생과 사립초 학생들을 전문적으로 지도하며, <br/>
-          원장 선생님이 직접 지도함으로써 각 <strong>학생의 잠재력</strong>을 최대한 이끌어냅니다.
-        </p>
-        <p className="text-lg leading-relaxed mb-2.5">
-          조재현 수학 학원 수업에서는 <strong>기본 개념을 확실히 다지는 것을 중요하게 생각</strong>하며, <br/>
-          아이들의 현재 수준과 목표에 따라 <strong>선행 학습과 심화 학습을 유연하게 진행</strong>합니다.<br/>
-          또한 학습 중 부족한 부분은 철저히 보완하고, 오답 관리를 통해 학습 공백을 최소화합니다. <br/>
-          아이들이 2~3시간 이상 집중하며 <strong>스스로 문제를 해결할 수 있는 힘</strong>을 기를 수 있도록 돕는 것도 중요한 목표 중 하나입니다.
-        </p>
+    <section className="mb-20">
+      <Title title="학원 소개" color="blue"/>
+      <div className="flex flex-col md:flex-row gap-[50px]">
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-[200px] h-[200px] bg-white rounded-full flex items-center justify-center shadow-3">
+            <Image src="/images/logos/logo_darkgreen.png" alt="introduction-1" width={120} height={120} className="object-cover"/>
+          </div>
+          <div className="font-bold NanumSquare text-center mt-5 leading-relaxed">
+            <span className="text-[#666]">작지만 강한 학원</span><br/>
+            <span className="text-xl font-extrabold">조재현 수학학원</span>
+          </div>
+        </div>
+        <div className="flex-1 flex flex-col gap-2.5 justify-center py-2.5">
+          <p className="text-lg leading-relaxed">
+            조재현수학은 <strong>{currentYear - 2005}년 이상의 학원 운영 경력</strong>을 바탕으로 초중등 학생들에게 최상의 맞춤형 수학 교육을 제공합니다. <br/>
+            단순히 수학 성적 향상만을 목표로 하지 않고, 아이들이 스스로 문제를 해결하며 <strong>깊이 있는 사고력</strong>을 키울 수 있도록 돕는 데 중점을 두고 있습니다.
+          </p>
+          <p className="text-lg leading-relaxed">
+            유아교육과 출신의 원장 선생님이 초중등 최상위권 학생과 사립초 학생들을 전문적으로 지도하며, <br/>
+            원장 선생님이 직접 지도함으로써 각 <strong>학생의 잠재력</strong>을 최대한 이끌어냅니다.
+          </p>
+          <p className="text-lg leading-relaxed">
+            조재현 수학 학원 수업에서는 <strong>기본 개념을 확실히 다지는 것을 중요하게 생각</strong>하며, <br/>
+            아이들의 현재 수준과 목표에 따라 <strong>선행 학습과 심화 학습을 유연하게 진행</strong>합니다.<br/>
+            또한 학습 중 부족한 부분은 철저히 보완하고, 오답 관리를 통해 학습 공백을 최소화합니다. <br/>
+            아이들이 2~3시간 이상 집중하며 <strong>스스로 문제를 해결할 수 있는 힘</strong>을 기를 수 있도록 돕는 것도 중요한 목표 중 하나입니다.
+          </p>
+        </div>
       </div>
     </section>
 
-    <Title title="학원 특징" color="blue"/>
     <section className="mb-20">
+      <Title title="수업 특징" color="blue"/>
       <div className="mt-4 grid sm:grid-cols-2 lg:grid-cols-3 gap-[30px]">
         {features.map((feature, index) => (
           <div key={index} className="sm:h-[300px] rounded-[20px] shadow-2 p-[10px] flex flex-col border border-[#eee]">
@@ -102,8 +112,8 @@ const IntroductionPage = () => {
       </div>
     </section>
 
-    <Title title="대상 학교" color="blue"/>
     <section className="mb-20 grid xl:grid-cols-2 gap-5 -mt-5">
+      <Title title="대상 학교" color="blue"/>
       <SchoolList type="초등학교" schools={["청원초", "상명초", "화랑초", "을지초", "상수초", "중계초", "상곡초", "당현초"]} />
       <SchoolList type="중학교" schools={["신상중", "상계중", "중계중", "창동중", "온곡중"]} />
     </section>
