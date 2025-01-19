c79374c58eb40161fc149e3ed247324e7e61cb79
Seowon Choi <chltjdnjs529@gmail.com>
2025-01-19 15:16:18 +0900

    feat: [FE] update highlight colors in ImprovedGradesSection for Elementary Math page;

--- frontend/app/(route)/common-math/elementary/page.tsx
+++ frontend/app/(route)/common-math/elementary/page.tsx
@@ -29,7 +29,7 @@ const improvedGradesList = [
     id: 1,
     student: "상곡초 6학년 A양",
     description: <div>
-      <span className="highlight-green"><b>‘레테 LOW’</b> 수준에서 <b>‘최상위 쎈’</b>까지 도달</span><br/>
+      <span className="highlight-yellow"><b>‘레테 LOW’</b> 수준에서 <b>‘최상위 쎈’</b>까지 도달</span><br/>
       현재 중등 과정 선행 중<br/>
       <b>고등 대비 심화 학습까지 진행!</b>
     </div>,
@@ -40,7 +40,7 @@ const improvedGradesList = [
     description: <div>
       초등 3학년부터 꾸준히 학습<br/>
       <b>수탐대회 100점 및 경시대회 금상 수상</b><br/>
-      <span className="highlight-green">심화 수학과 중등 수학 병행</span>하며 안정적인 성장 중!
+      <span className="highlight-yellow">심화 수학과 중등 수학 병행</span>하며 안정적인 성장 중!
     </div>,
   },
   {
@@ -49,7 +49,7 @@ const improvedGradesList = [
     description: <div>
       등록 <b>6개월</b> 만에 <b>초등 5학년 후행 학습 완성</b><br/>
       <b>초등 6학년 심화</b> 진도 완료<br/>
-      <span className="highlight-green">고학년 사고력 수학과 중등 선행</span> 완료 예정!
+      <span className="highlight-yellow">고학년 사고력 수학과 중등 선행</span> 완료 예정!
     </div>,
   },
   {
@@ -58,7 +58,7 @@ const improvedGradesList = [
     description: <div>
       사고력과 연산이 약했던 학생,<br/>
       <b>학습 습관을 개선</b>하여 1년간 연산과 교과 과정을 병행<br/>
-      <span className="highlight-green">현재 초등 6학년 <b>최상위 문제 풀이까지 가능!</b></span>
+      <span className="highlight-yellow">현재 초등 6학년 <b>최상위 문제 풀이까지 가능!</b></span>
     </div>,
   },
   {
@@ -66,7 +66,7 @@ const improvedGradesList = [
     student: "상수초 3학년 E군",
     description: <div>
       연산이 느리고 개념 이해가 부족했던 학생,<br/>
-      <span className="highlight-green">교구와 시각 자료</span>를 활용한 학습하여<br/>
+      <span className="highlight-yellow">교구와 시각 자료</span>를 활용한 학습하여<br/>
       학기 중반에 <b>교과 연산 100문제 테스트에서 만점 기록!</b>
     </div>,
   },
