361bbe350447f7709eb8d73133573061e484c9e9
Seowon Choi <chltjdnjs529@gmail.com>
2025-01-19 15:15:20 +0900

    feat: [FE] update highlight styles in ImprovedGradesSection for Elementary and Middle Math pages; replace existing highlight class with new color-coded classes for better visual distinction

--- frontend/app/(route)/common-math/middle/page.tsx
+++ frontend/app/(route)/common-math/middle/page.tsx
@@ -30,7 +30,7 @@ const improvedGradesList = [
     student: "신상중 2학년 A양",
     description: <div>
       여름방학에 찾아온 중1<br/>
-      <span className="highlight">9개월만에 1학기 복습 + 현행 + 선행</span> 끝내고<br/>
+      <span className="highlight-green">9개월만에 1학기 복습 + 현행 + 선행</span> 끝내고<br/>
       <b>2학년 첫 중간고사 100점!</b>
     </div>,
   },
@@ -39,7 +39,7 @@ const improvedGradesList = [
     student: "상계중 2학년 B양",
     description: <div>
       학원 안 다녀 기초 없던 중1<br/>
-      <span className="highlight">6개월만에 1년치 총복습 + 현행</span>하고<br/>
+      <span className="highlight-green">6개월만에 1년치 총복습 + 현행</span>하고<br/>
       <b>2학년 첫 중간고사 100점!</b>
     </div>,
   },
@@ -48,7 +48,7 @@ const improvedGradesList = [
     student: "신상중 2학년 C군",
     description: <div>
       초등과정 하나도 모르던 중1<br/>
-      <span className="highlight">기본부터 시작해 응용심화 마스터</span>하더니<br/>
+      <span className="highlight-green">기본부터 시작해 응용심화 마스터</span>하더니<br/>
       <b>2학년 첫 중간고사 95점!</b>
     </div>,
   },
@@ -57,7 +57,7 @@ const improvedGradesList = [
     student: "신상중 3학년 D양",
     description: <div>
       초5 과정부터 다시 배우고 있는 중3<br/>
-      <span className="highlight">3개월만에 몇년치 복습</span>하고<br/>
+      <span className="highlight-green">3개월만에 몇년치 복습</span>하고<br/>
       <b>3학년 첫 중간고사 70점!</b>
     </div>,
   },
@@ -66,7 +66,7 @@ const improvedGradesList = [
     student: "상계중 2학년 B양",
     description: <div>
       4개월 만에 45점 상승<br/>
-      <span className="highlight">첫 중간고사에서 <b>93점</b> 달성</span><br/>
+      <span className="highlight-green">첫 중간고사에서 <b>93점</b> 달성</span><br/>
       <b>학교 내신 완벽 대비 + 상위권 진입 성공</b>
     </div>,
   },
