34737cf4fdc79512fe54562200e5429773219300
Seowon Choi <chltjdnjs529@gmail.com>
2025-01-22 16:51:30 +0900

    feat: [FE] enhance RoundButton component; add customizable width and height properties, and improve hover effects for better responsiveness and user experience

--- frontend/app/_components/Button/RoundButton.tsx
+++ frontend/app/_components/Button/RoundButton.tsx
@@ -7,28 +7,34 @@ export interface RoundButtonProps {
   onClick?: () => void;
   color?: "yellow" | "green";
   isActive?: boolean;
+  width?: string | undefined;
+  height?: string | undefined;
   props?: ButtonHTMLAttributes<HTMLButtonElement>;
 }
 
-const RoundButton = ({ label, onClick, color = "green", isActive = false, props } : RoundButtonProps) => {
+const RoundButton = ({ label, onClick, color = "green", isActive = false, width, height, props } : RoundButtonProps) => {
   // 버튼 색상에 따라 클래스 적용
   const colorClassMap = {
     yellow: {
       activeClass: "bg-yellow-3 text-black",
-      reactedClass: "hover:bg-yellow-3 hover:text-black active:bg-yellow-3 active:text-black",
+      reactedClass: "md:hover:bg-yellow-3 md:hover:text-black active:bg-yellow-3 active:text-black",
     },
     green: {
       activeClass: "bg-green-1 text-white",
-      reactedClass: "hover:bg-green-1 hover:text-white active:bg-green-1 active:text-white",
+      reactedClass: "md:hover:bg-green-1 md:hover:text-white active:bg-green-1 active:text-white",
     },
   };
 
   return <button 
     className={`
-      font-semibold text-sm px-[10px] py-[5px] rounded-full w-[130px] text-center transition-all duration-300 NanumSquare whitespace-nowrap 
+      flex items-center justify-center font-semibold text-sm px-[15px] py-[5px] rounded-full text-center transition-all duration-300 NanumSquare whitespace-nowrap 
       ${colorClassMap[color].reactedClass}
       ${isActive ? colorClassMap[color].activeClass : "bg-[#F3F3F3] text-[#555]"}
     `}
+    style={{
+      width: width ? width : "130px",
+      height: height ? height : "auto",
+    }}
     onClick={onClick ? onClick : () => {}}
     {...props}
   >
