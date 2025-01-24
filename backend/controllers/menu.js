c44a95e4f44117e2cb371e7aca36d6e8b2aedfe7
Seowon Choi <chltjdnjs529@gmail.com>
2025-01-24 14:46:56 +0900

    feat: [BE] implement CRUD operations for Menu with Swagger documentation

--- backend/controllers/menu.js
+++ backend/controllers/menu.js
@@ -12,3 +12,42 @@ exports.getMenuList = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getMenu = async (req, res, next) => {
+  try {
+    const menu = await Menu.findByPk(req.params.id);
+    if (!menu) {
+      return res.status(404).json({ message: '메뉴 정보를 찾을 수 없습니다.' });
+    }
+    res.status(200).json(menu);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createMenu = async (req, res, next) => {
+  try {
+    const menu = await Menu.create(req.body);
+    res.status(201).json(menu);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateMenu = async (req, res, next) => {
+  try {
+    const menu = await Menu.update(req.body, { where: { id: req.params.id } });
+    res.status(200).json(menu);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteMenu = async (req, res, next) => {
+  try {
+    await Menu.destroy({ where: { id: req.params.id } });
+    res.status(204).json({ message: '메뉴가 삭제되었습니다.' });
+  } catch (error) {
+    next(error);
+  }
+};
