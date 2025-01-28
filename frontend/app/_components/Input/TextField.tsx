06336e46f2ac1abcec87b3f3e44731dbea1710b2
Seowon Choi <chltjdnjs529@gmail.com>
2025-01-28 14:11:39 +0900

    feat: [FE] add optional description to TextField component

--- frontend/app/_components/Input/TextField.tsx
+++ frontend/app/_components/Input/TextField.tsx
@@ -7,6 +7,7 @@ import styles from "./Input.module.css";
 interface TextFieldProps {
   label: string;
   placeholder: string;
+  description?: string;
   inputType: HTMLInputTypeAttribute;
   value?: string;
   onChange?: (value: string) => void;
@@ -17,31 +18,34 @@ interface TextFieldProps {
   props?: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 }
 
-const TextField = ({ label, placeholder, inputType, value, onChange, buttonLabel, buttonProps, register, error, props }: TextFieldProps) => {
+const TextField = ({ label, placeholder, description, inputType, value, onChange, buttonLabel, buttonProps, register, error, props }: TextFieldProps) => {
 
   return (
     <div className="w-full">
       <label className="text-sm Montserrat ml-1">{label}</label>
-      <div className="flex items-center mb-4 mt-1">
-        <input 
-          type={inputType} 
-          className={`flex-1 h-10 px-3 rounded-[8px] bg-white shadow-2 border-[2px] border-transparent ${styles.textfield} ${error ? 'border-red-2' : ''}`} 
-          placeholder={placeholder} 
-          value={value}
-          onChange={(e) => onChange && onChange(e.target.value)}
-          {...register}
-          {...props}
-        />
-        {buttonProps && (
-          <ReactiveButton 
-            props={{
-              ...buttonProps, 
-              className: "bg-green-1 text-white shadow-2 px-3 py-2 rounded-[8px] whitespace-nowrap ml-2 max-w-[100px]"
-            }}
-          >
-              {buttonLabel}
-          </ReactiveButton>
-        )}
+      <div className="mb-4 mt-1">
+        <div className="flex items-center">
+          <input 
+            type={inputType} 
+            className={`flex-1 h-10 px-3 rounded-[8px] bg-white shadow-2 border-[2px] border-transparent ${styles.textfield} ${error ? 'border-red-2' : ''}`} 
+            placeholder={placeholder} 
+            value={value}
+            onChange={(e) => onChange && onChange(e.target.value)}
+            {...register}
+            {...props}
+          />
+          {buttonProps && (
+            <ReactiveButton 
+              props={{
+                ...buttonProps, 
+                className: "bg-green-1 text-white shadow-2 px-3 py-2 rounded-[8px] whitespace-nowrap ml-2 max-w-[100px]"
+              }}
+            >
+                {buttonLabel}
+            </ReactiveButton>
+          )}
+        </div>
+        {description && <p className="text-sm text-[#777] mt-1.5 ml-2">* {description}</p>}
       </div>
       {error && error.message && 
         <FormError errorMessage={error.message} />
