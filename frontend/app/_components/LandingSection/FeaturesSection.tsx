90d014fb8fac583630de891828f58321c8035999
Seowon Choi <chltjdnjs529@gmail.com>
2025-01-29 14:46:33 +0900

    feat: [FE] enhance FeaturesSection with dynamic grid layout

--- frontend/app/_components/LandingSection/FeaturesSection.tsx
+++ frontend/app/_components/LandingSection/FeaturesSection.tsx
@@ -13,6 +13,8 @@ interface FeaturesSectionProps {
 const FeaturesSection = ({ title, subtitle, features, backgroundColor }: FeaturesSectionProps) => {
   const { ref, isIntersected } = useIntersection(1);
 
+  const gridCols = features.length % 4 === 0 ? "lg:grid-cols-4" : features.length % 3 === 0 ? "lg:grid-cols-3" : "lg:grid-cols-2";
+
   const bgColor = backgroundColor === "green" ? "bg-[#ecf6e6]" : "bg-[#fffbe2]";
   return <section className={`flex justify-center items-center w-full py-20 md:pt-[120px] md:pb-[140px] ${bgColor}`}>
     <div className="2xl:w-[80rem] xl:w-[72rem] lg:w-[56rem] md:w-[48rem] sm:w-[36rem] w-full min-w-[300px] px-5">
@@ -22,7 +24,7 @@ const FeaturesSection = ({ title, subtitle, features, backgroundColor }: Feature
       <div className="text-center text-lg sm:text-2xl mt-2.5">
         {title}
       </div>
-      <div ref={ref} className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[10px] md:gap-[30px] mt-[60px]">
+      <div ref={ref} className={`grid grid-cols-1 md:grid-cols-2 gap-[10px] md:gap-[30px] mt-[60px] ${gridCols}`}>
         {features.map((feature, index) => (
           <div 
             key={index} 
