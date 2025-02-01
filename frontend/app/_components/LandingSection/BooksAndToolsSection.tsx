3197466f4d970c150dfeeac305390ed1c3d0cf2c
Seowon Choi <chltjdnjs529@gmail.com>
2025-02-01 19:51:16 +0900

    feat: [FE] Add intersection and animation effects to BooksAndToolsSection

--- frontend/app/_components/LandingSection/BooksAndToolsSection.tsx
+++ frontend/app/_components/LandingSection/BooksAndToolsSection.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import useIntersection from "@/app/_hooks/useIntersection";
 import Image from "next/image";
+import { useMediaQuery } from "usehooks-ts";
 
 interface SectionProps {
   title: string;
@@ -13,7 +15,7 @@ interface SectionProps {
 }
 
 const Section = ({ title, imageSrc, imageClass, items, features, itemType, color }: SectionProps) => (
-  <div className="flex flex-col gap-5 p-2.5 pb-6 rounded-[30px] shadow-3 NanumSquare">
+  <div className="h-full flex flex-col gap-5 p-2.5 pb-6 rounded-[30px] shadow-3 NanumSquare">
     <div className={`relative w-full aspect-[1.8] rounded-[22px] overflow-hidden ${itemType === "book" ? (color === "green" ? "bg-green-4" : "bg-yellow-4") : ""}`}>
       <Image 
         src={imageSrc} 
@@ -58,28 +60,37 @@ interface BooksAndToolsSectionProps {
 }
 
 const BooksAndToolsSection = ({ title, books, tools, featuresOfBooks, featuresOfTools, color }: BooksAndToolsSectionProps) => {
-  return <div className="flex justify-center items-center w-full">
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const { ref, isIntersected } = useIntersection(isMobile ? 0.2 : 0.8);
+
+  return <div ref={ref} className="flex justify-center items-center w-full">
     <div className="2xl:w-[80rem] xl:w-[72rem] lg:w-[56rem] md:w-[48rem] sm:w-[36rem] w-full max-w-[995px] min-w-[300px] px-5 md:py-[120px] py-[60px]">
-      <h2 className={`text-2xl font-extrabold NanumSquare text-center mb-12 ${color === "yellow" ? "text-yellow-5" : "text-green-2"}`}>{title}에서는 어떤 교재와 교구를 사용할까요?</h2>
+      <h2 className={`text-2xl font-extrabold NanumSquare text-center mb-12 ${color === "yellow" ? "text-yellow-5" : "text-green-2"}`}>
+        {title}에서는 <br className="block md:hidden"/>어떤 교재와 교구를 사용할까요?
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px]">
-        <Section 
-          title={title}
-          imageSrc="/images/sigma-class-books.png" 
-          imageClass="object-contain -rotate-45 scale-[250%]" 
-          items={books} 
-          itemType="book"
-          features={featuresOfBooks}
-          color={color}
-        />
-        <Section 
-          title={title}
-          imageSrc="/images/learning-tools-2.png" 
-          imageClass="object-cover" 
-          items={tools} 
-          itemType="tool"
-          features={featuresOfTools}
-          color={color}
-        />
+        <div className={isIntersected ? (isMobile ? "fade-in-bottom" : "swing-in-right-fwd") : "opacity-0"}>
+          <Section 
+            title={title}
+            imageSrc="/images/sigma-class-books.png" 
+            imageClass="object-contain -rotate-45 scale-[250%]" 
+            items={books} 
+            itemType="book"
+            features={featuresOfBooks}
+            color={color}
+          />
+        </div>
+        <div className={isIntersected ? (isMobile ? "fade-in-bottom" : "swing-in-left-fwd") : "opacity-0"}>
+          <Section 
+            title={title}
+            imageSrc="/images/learning-tools-2.png" 
+            imageClass="object-cover" 
+            items={tools} 
+            itemType="tool"
+            features={featuresOfTools}
+            color={color}
+          />
+        </div>
       </div>
     </div>
   </div>
