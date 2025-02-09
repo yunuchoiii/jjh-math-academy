562f47ab9af1b90e43eecf9cf7116365014580ea
Seowon Choi <chltjdnjs529@gmail.com>
2025-02-09 14:42:41 +0900

    feat: [BE] Update database configuration to use environment variables

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,11 +1,17 @@
 const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const config = require('../config/config');
+const env = process.env.NODE_ENV;
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config[env].database, 
+  config[env].username, 
+  config[env].password, 
+  config[env]
+);
+
 
 db.sequelize = sequelize;
 
